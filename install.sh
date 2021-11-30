aa8ad2ab5940f0cff82ba7392fc2df17f6ceab40
HoolaBoola <ale@hoolaboola.fi>
2021-11-30 03:22:55 +0200

    Added installation script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
+if [[ -z "$version" ]]
+then
+    echo "Python not installed!" 
+    exit
+fi
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+# cd to project root, where install.sh is located
+# (in case the script is called from elsewhere)
+cd $SCRIPTPATH
+
+VENV_DIR=${SCRIPTPATH}/venv
+if [ -d "$VENV_DIR" ]; then
+    echo "venv exists, skipping..."
+else 
+    echo "venv does not exist, creating..."
+    python3 -m venv venv
+fi
+
+echo "Sourcing venv..."
+source venv/bin/activate
+
+pip install -r requirements.txt
+deactivate
+
+exit
