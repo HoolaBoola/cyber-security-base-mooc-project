e174c01689b1f0383f0fc97b68cab676beb4d4eb
HoolaBoola <ale@hoolaboola.fi>
2021-11-30 03:34:26 +0200

    Added launch script

---
+++ launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+VENV_DIR=${SCRIPTPATH}/venv
+if [ ! -d "$VENV_DIR" ]; then
+    echo "venv does not exist, exiting..."
+    exit
+fi
+
+source ${SCRIPTPATH}/venv/bin/activate
+
+python3 $SCRIPTPATH/website/manage.py runserver
